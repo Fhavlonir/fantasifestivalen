d5f7eda923a6ef94999964cef80ee6f273b0a4f6
Philip Johansson <filip.johansson1996@gmail.com>
2023-01-11 18:09:00 +0100

    added about page, fixed a bit of formatting

---
+++ lib/screens/aboutpage/aboutpage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class AboutPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Om appen'),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Text('Hej!',
+                  style: Theme.of(context).textTheme.headline1
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                constraints: BoxConstraints(maxWidth:200),
+                child: ClipOval(
+                  child: SizedBox.fromSize(     
+                    size: Size.fromRadius(100), 
+                    child: CachedNetworkImage(imageUrl: 'https://pbs.twimg.com/profile_images/1373325891384786946/IkVLOrwN_400x400.jpg'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                constraints: BoxConstraints(maxWidth: 400),
+                child: Text(
+                  'Jag som gjort den här appen heter Philip Johansson, jag bor i Lund och pluggar på LTH. Hoppas att du gillar den.\nAppen är publicerad under licensen GNU Affero General Public License version 3.\nDen är byggd med hjälp av Flutter, hostad med hjälp av Github Pages och Supabase.\nBilderna kommer från Wikimedia Commons, oändliga tack till alla som bidragit med dessa, särskilt Jonatan Svensson Glad, AnnCatrin Blyckerts och Daniel Åhs Karlsson.'),
+              )
+            ]
+          )]
+        ),
+      );
+  }
+}
