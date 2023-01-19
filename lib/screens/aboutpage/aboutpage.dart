80f059dffcd51407c689c95d3ace137e07d79880
Philip Johansson <filip.johansson1996@gmail.com>
2023-01-19 19:22:44 +0100

    Added link to source code in order to be complied to the license I chose myself.

--- lib/screens/aboutpage/aboutpage.dart
+++ lib/screens/aboutpage/aboutpage.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
+
+  final Uri _url = Uri.parse('https://github.com/Fhavlonir/fantamello');
+  
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(_url)) {
+      throw 'Could not launch $_url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +41,10 @@ class AboutPage extends StatelessWidget {
               Container(
                 margin: EdgeInsets.all(10),
                 constraints: BoxConstraints(maxWidth: 400),
-                child: Text(
-                  'Jag hoppas du gillar Fantamello! Jag som gjort den heter Philip Johansson, jag bor i Lund och pluggar på LTH.\nAppen är publicerad under licensen GNU Affero General Public License version 3.\nDen är byggd med hjälp av Flutter, hostad med hjälp av Github Pages och Supabase.\nBilderna kommer från Wikimedia Commons, oändliga tack till alla som bidragit med dessa, särskilt Jonatan Svensson Glad, AnnCatrin Blyckerts och Daniel Åhs Karlsson.\n\nOm du har frågor eller feedback kan du kontakta mig på philip@fantamello.se'),
+                child: Column( children: [
+                  Text('Jag hoppas du gillar Fantamello! Jag som gjort den heter Philip Johansson, jag bor i Lund och pluggar på LTH.\nAppen är publicerad under licensen GNU Affero General Public License version 3.\nDen är byggd med hjälp av Flutter, hostad med hjälp av Github Pages och Supabase.\nBilderna kommer från Wikimedia Commons, oändliga tack till alla som bidragit med dessa, särskilt Jonatan Svensson Glad, AnnCatrin Blyckerts och Daniel Åhs Karlsson.\n\nOm du har frågor eller feedback kan du kontakta mig på philip@fantamello.se'),
+                  SizedBox(height:20),
+                  TextButton(child: const Text('Källkod och licens finns att tillgå via\ngithub.com/Fhavlonir/fantamello'), onPressed: _launchUrl)]),
               )
             ]
           )]
